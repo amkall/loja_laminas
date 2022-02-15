f413846c4752e4897cd652c16806a8d86c4af982
amkall <brunogt03826@gmail.com>
2022-02-15 14:01:40 -0300

    'table'

---
+++ module/Loja/src/Model/ClienteTable.php
@@ -0,0 +1,67 @@
+<?php
+namespace Album\Model;
+
+use RuntimeExeption;
+use Laminas\Db\TableGateway\TableGatewayInterface;
+
+class ClienteTable{
+
+    private $tableGateway;
+
+    public function __construct(TableGatewayInterface $tableGateway){
+        
+        $this->tableGateway = $tableGateway;
+
+    }
+
+    public function getAll(){
+        return $this->tableGateway->select();
+    }
+
+    public function getCliente($id){
+
+        $id     = (int) $id;
+        $rowset = this->tableGateway->select(['ID_CLIENTE' => $id]);
+        $row    = $rowset->current();
+
+        if(! $row){
+            throw new RuntimeException(sprintf(
+                'Could not find row with identifier %d',
+                $id
+            ));
+        }
+
+        return $row;
+
+    }
+
+    public function saveCliente(Cliente $cliente){
+        
+        $data = [
+            'ID_CLIENTE' => $cliente->id_cliente;
+            'LOGIN'      => $cliente->login;
+            'SENHA'      => $cliente->senha;
+        ];
+
+        $id_cliente = (int) $cliente->id_cliente;
+        
+        // id_cliente nao e passado, se for uma atualização nao entra
+        
+        if($id_cliente === 0){
+            $this->tableGateway->insert($data);
+            
+            return;
+        }
+
+        try {
+            $this->getCliente($id_cliente);
+        } catch (RuntimeException $e) {
+            throw new RuntimeException(sprintf(
+                'Cannot update album with identifier %d; does not exist',
+                $id_cliente
+            ));
+        }
+
+        $this->tableGateway->update($data, ['ID_CLIENTE' => $id_cliente]);
+    }
+}
